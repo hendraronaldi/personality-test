3ae3c2b44bd95720d17e3208b61d04e2bbd8a020
hendraronaldi <hendraronaldi@hotmail.com>
2019-05-14 01:15:46 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"work/personality-test/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	ConfigRuntime()
+	StartGin()
+}
+
+func ConfigRuntime() {
+	nuCPU := runtime.NumCPU()
+	runtime.GOMAXPROCS(nuCPU)
+	fmt.Printf("Running with %d CPUs\n", nuCPU)
+	fmt.Println("this apps working completly fine on environment", os.Getenv("DOT_ENV"))
+}
+
+func StartGin() {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+
+	router.Use(routes.CORSMiddleware())
+	routes.SetupRouter(router)
+	router.Run(":8000") //port for localhost:8000
+}
