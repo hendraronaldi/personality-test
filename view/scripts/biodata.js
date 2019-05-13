d91f5faca33c1ef0bda91783b77ffacead0906fd
hendraronaldi <hendraronaldi@hotmail.com>
2019-05-14 01:59:50 +0700

    biodata view

--- view/scripts/biodata.js
+++ view/scripts/biodata.js
@@ -1,3 +1,15 @@
 import biodata from './config/biodata.js';
 
-console.log(biodata);
+function biodataTemplate() {
+    biodata.forEach((field) => {
+        $('#biodata').append(
+            `<div class="input-group no-border input-lg">
+                <input type="`+field.type+`" class="form-control" placeholder="`+field.label+`" required>
+            </div>`
+        );
+    })
+}
+
+$(document).ready(function(){
+    biodataTemplate();
+});
