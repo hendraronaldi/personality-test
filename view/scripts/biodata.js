265e9213f10bb2a48c777277e361c663c2c9ec4a
hendraronaldi <hendraronaldi@hotmail.com>
2019-05-14 16:10:55 +0700

    biodata template

--- view/scripts/biodata.js
+++ view/scripts/biodata.js
@@ -1,15 +1,38 @@
 import biodata from './config/biodata.js';
 
+var userBiodata;
+
 function biodataTemplate() {
+    $("#card-form").append(`<form id="form-biodata" class="form">
+        <div class="card-header text-center">
+            <h1>Biodata</h1>
+        </div>
+        <div id="biodata" class="card-body">
+        </div>
+        <div class="card-footer text-center">
+            <input type="submit" class="btn btn-primary btn-round btn-lg btn-block" value="Submit" />
+        </div>
+    </form>`);
+
     biodata.forEach((field) => {
         $('#biodata').append(
             `<div class="input-group no-border input-lg">
-                <input type="`+field.type+`" class="form-control" placeholder="`+field.label+`" required>
+                <input name="`+field.name+`" type="`+field.type+`" class="form-control" placeholder="`+field.label+`" required>
             </div>`
         );
     })
 }
 
+function saveBiodata() {
+    $("#form-biodata").submit(function(e){
+        userBiodata = $("#form-biodata").serializeArray();
+
+        
+        e.preventDefault();
+    });
+}
+
 $(document).ready(function(){
-    biodataTemplate();
+    // biodataTemplate();
+    // saveBiodata();
 });
--- view/scripts/config/biodata.js
+++ view/scripts/config/biodata.js
@@ -1,22 +1,27 @@
 export default
 [
     {
+        "name": "name",
         "label": "Nama Lengkap",
         "type": "text"
     },
     {
+        "name": "birthdate",
         "label": "Tanggal Lahir",
         "type": "date"
     },
     {
+        "name": "phone",
         "label": "No. HP",
         "type": "text"
     },
     {
+        "name": "email",
         "label": "Email",
         "type": "email"
     },
     {
+        "name": "dreams",
         "label": "Pekerjaan Impian",
         "type": "text"
     }
